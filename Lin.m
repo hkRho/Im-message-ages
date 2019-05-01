b551fa613616f9c5906305d01e20b2b68aa6354c
Manu Patil <mpatil99@gmail.com>
2019-05-01 01:05:38 -0400

    Algorithm generally works

--- Lin.m
+++ Lin.m
@@ -1,22 +1,35 @@
+% clear
+% output = [[2,1];[1,2]]
+% [S,V,D] = svd(output)
+clear
+I = imread('SquareCat.bmp');
+% figure
+% imshow(I);
+% figure
 
-% SquareCat;
-% 
-% output =int16 (SquareCat(:,:,1)./3 + SquareCat(:,:,2)./3 + SquareCat(:,:,3)./3);
-% output
-% 
-% output(1:1,1:2);
-% 
+output = single(I(:,:,1)./3 + I(:,:,2)./3 + I(:,:,3)./3);
+% imshow(mat2gray(uint8(output)));
+
+% figure
+output(1:1,1:2);
+[S,V,D] = svd(output);
+A1 = S*V*D';
+
+imshow( mat2gray(uint8(A1)));
+protected_rows = 1;
 M = [-1,1,-1,-1,-1,-1,-1,-1,1,1,-1,-1,1,1,1,-1,-1,-1,1,-1,-1];
-
-output =( [[1,2,1];[3,6,3];[1,8,9]])
+M =[1,1,1];
+output = S;
 count = 1;
-for i = 2:size(output,1)
+for i = 1+protected_rows :size(output,1)
     b = (zeros(i-1,1));
     A = zeros(i-1,i-1);
     for j = 1:(size(output,1)-i+1)
         output(i,j)= output(i,j).*M(count);
         count = count +1;
-        
+        if count > size(M,2)
+            count = 1;
+        end        
         b = b+ output(i,j) .* output(1:i-1,j:j);
     end
 
@@ -28,7 +41,11 @@ for i = 2:size(output,1)
     end
     
 end
-output
+
+figure
+A2 = output*V*D'
+uint8(A1)-uint8(A2)
+imshow( mat2gray(uint8(A2)));
 
 
 
